4c29491cf2e41069c7fc67c0f09d51b3f1b136f4
Nolan <noltron000@gmail.com>
2021-07-07 19:37:16 -0400

    setting up assets & components directory

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from '../Button/Button';
+import './Header.css';
 
 interface HeaderProps {
   user?: {};
