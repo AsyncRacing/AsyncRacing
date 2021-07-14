bb26556aa1b2df22d39e299d416c6834a130e42c
Nolan <noltron000@gmail.com>
2021-07-14 13:50:39 -0400

    UploadButton: (hook III) moving helper outside
    Breaking up the problem should help with finding a solution.

--- src/components/UploadButton/useFileContent.ts
+++ src/components/UploadButton/useFileContent.ts
@@ -4,44 +4,44 @@ import React, {
   useState,
 } from 'react';
 
+// Create a file-reader function here.
+const readFileContentString = (file: null | File): null | string => {
+  // Keep track of the file's data, which will be returned.
+  let fileData: string | null = null
+
+  // Use the "FileReader" JS Web API to read variables with type "File".
+  const reader: FileReader = new FileReader()
+  // Assign properties to various functions to execute onEvent.
+  // This will set up the file reader for use.
+  reader.onload = (onLoadEvent) => {
+    // Ensure the event target...exists!
+    if (onLoadEvent.target === null) {
+      return
+    }
+    /*
+      NOTE: TypeScript is giving an unneeded warning here.
+      Because reader.onload will only ever run from reader.readAsText(),
+      fileData will only ever end up being a string or null.
+    */ // @ts-expect-error
+    fileData = onLoadEvent.target.result
+  }
+
+  if (file !== null) {
+    // Use the reader to obtain fileData.
+    reader.readAsText(file)
+    console.log(fileData)
+  }
+
+  // Return the fileData obtained from the reader.
+  return fileData
+}
+
 const useFileContentString = (
   defaultFile: null | File = null
 ): [
   string | null,
-  Dispatch<SetStateAction<any>>
+  Dispatch<SetStateAction<any>>,
 ] => {
-  // Create a file-reader function here.
-  const readFileContentString = (file: null | File): null | string => {
-    // Keep track of the file's data, which will be returned.
-    let fileData: string | null = null
-
-    // Use the "FileReader" JS Web API to read variables with type "File".
-    const reader: FileReader = new FileReader()
-    // Assign properties to various functions to execute onEvent.
-    // This will set up the file reader for use.
-    reader.onload = (onLoadEvent) => {
-      // Ensure the event target...exists!
-      if (onLoadEvent.target === null) {
-        return
-      }
-      /*
-        NOTE: TypeScript is giving an unneeded warning here.
-        Because reader.onload will only ever run from reader.readAsText(),
-        fileData will only ever end up being a string or null.
-      */ // @ts-expect-error
-      fileData = onLoadEvent.target.result
-    }
-
-    if (file !== null) {
-      // Use the reader to obtain fileData.
-      reader.readAsText(file)
-      console.log(fileData)
-    }
-
-    // Return the fileData obtained from the reader.
-    return fileData
-  }
-
   // Use react state for the hook!
   // Prepare the proper fileContent from the given file.
   const [fileContentString, setFile] = useState(readFileContentString(defaultFile))
