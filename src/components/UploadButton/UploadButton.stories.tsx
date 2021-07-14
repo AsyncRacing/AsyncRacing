9330307f5ece9eab567bbb1bc82dbc8e91d0fefa
Nolan <noltron000@gmail.com>
2021-07-14 16:21:40 -0400

    implementing useEffect for the file reader hook
    I spoke with Thomas about useEffect and implementing it into the upload button. It looks like this solution is superior to the solution I have in the main upload-button branch. I plan on revisiting this later!

--- src/components/UploadButton/UploadButton.stories.tsx
+++ src/components/UploadButton/UploadButton.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import UploadButton from './UploadButton';
 import useFileContentString from './useFileContent';
@@ -12,17 +12,28 @@ export default {
 } as ComponentMeta<typeof UploadButton>;
 
 const Template: ComponentStory<typeof UploadButton> = (args) => {
-  const [fileContentString, setFileContentString] = useFileContentString()
+  console.log("Rendering Story")
+  const [file, setFile] = useState<File | null>(null)
+  const [name, setName] = useState("")
+  const fileContentString = useFileContentString(file)
+
   return (
     <>
       <UploadButton
         {...args}
-        setFile={setFileContentString}
+        setFile={setFile}
       />
       <textarea
         value={fileContentString ?? ""}
         disabled
         readOnly
+      />
+      <input
+        type='text'
+        value={name}
+        onChange={(e)=>{
+          setName(e.target.value)
+        }}
       />
     </>
   );
--- src/components/UploadButton/useFileContent.ts
+++ src/components/UploadButton/useFileContent.ts
@@ -1,55 +1,54 @@
 import React, {
   Dispatch,
   SetStateAction,
+  useEffect,
   useState,
 } from 'react';
 
 const useFileContentString = (
-  defaultFile: null | File = null
-): [
-  string | null,
-  Dispatch<SetStateAction<any>>
-] => {
-  // Create a file-reader function here.
-  const readFileContentString = (file: null | File): null | string => {
-    // Keep track of the file's data, which will be returned.
-    let fileData: string | null = null
+  file: null | File = null
+): string | null => {
 
-    // Use the "FileReader" JS Web API to read variables with type "File".
-    const reader: FileReader = new FileReader()
-    // Assign properties to various functions to execute onEvent.
-    // This will set up the file reader for use.
-    reader.onload = (onLoadEvent) => {
-      // Ensure the event target...exists!
-      if (onLoadEvent.target === null) {
-        return
+  const [fileContent, setFileContent] = useState<string | null>(null)
+
+
+
+  useEffect(() => {
+    // Create a file-reader function here.
+    const readFileContent = (file: null | File) => {
+      // Keep track of the file's data, which will be returned.
+
+      // Use the "FileReader" JS Web API to read variables with type "File".
+      const reader: FileReader = new FileReader()
+      // Assign properties to various functions to execute onEvent.
+      // This will set up the file reader for use.
+      reader.onload = (onLoadEvent) => {
+        // Ensure the event target...exists!
+        if (onLoadEvent.target === null) {
+          return
+        }
+        /*
+          NOTE: TypeScript is giving an unneeded warning here.
+          Because reader.onload will only ever run from reader.readAsText(),
+          fileData will only ever end up being a string or null.
+        */
+        const fileData: string | null = onLoadEvent.target.result as string
+        setFileContent(fileData)
       }
-      /*
-        NOTE: TypeScript is giving an unneeded warning here.
-        Because reader.onload will only ever run from reader.readAsText(),
-        fileData will only ever end up being a string or null.
-      */ // @ts-expect-error
-      fileData = onLoadEvent.target.result
+
+
+      if (file !== null) {
+        setFileContent("...loading")
+        // Use the reader to obtain fileData.
+        reader.readAsText(file)
+      }
     }
+    readFileContent(file)
+  }, [file])
 
-    if (file !== null) {
-      // Use the reader to obtain fileData.
-      reader.readAsText(file)
-      console.log(fileData)
-    }
-
-    // Return the fileData obtained from the reader.
-    return fileData
-  }
-
-  // Use react state for the hook!
-  // Prepare the proper fileContent from the given file.
-  const [fileContentString, setFile] = useState(readFileContentString(defaultFile))
-  // Prepare the proper setFileContent function from the given setFile function.
-  const setFileContentString = (file: null | File) => setFile(readFileContentString(file))
   // Return both the starting value and the setting function,
   // which also both use my custom readFileContentString() function.
-  return [fileContentString, setFileContentString]
+  return fileContent
 }
 
 export default useFileContentString;
