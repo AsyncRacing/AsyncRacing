97326daa8085635022307615b01d8438b16f098f
Thomas Sarlandie <thomas@sarlandie.net>
2021-07-15 16:12:14 -0700

    refactor: switch to viewState instead of viewPort (new recommended style) + clean typescript typings to not use any

--- src/components/Map/ChallengeMap.tsx
+++ src/components/Map/ChallengeMap.tsx
@@ -1,80 +1,113 @@
-import * as React from 'react';
-import {InteractiveMap, FlyToInterpolator, WebMercatorViewport, Marker} from 'react-map-gl';
-import DeckGL from '@deck.gl/react'
-import {PathLayer} from '@deck.gl/layers'
+import * as React from "react";
+import {
+  InteractiveMap,
+  FlyToInterpolator,
+  WebMercatorViewport,
+  Marker,
+} from "react-map-gl";
+import DeckGL from "@deck.gl/react";
+import { PathLayer } from "@deck.gl/layers";
 // 3rd-party easing functions
 // import d3 from 'd3-ease';
-import flatpin from './assets/images/red-pin.png'
+import flatpin from "./assets/images/red-pin.png";
+import { ViewState } from "react-map-gl/src/mapbox/mapbox";
 
 export interface Track {
-  name: string,
-  color: [red: number, green: number, blue: number]
-  path: Array<[lon:number, lat: number]>
+  name: string;
+  color: [red: number, green: number, blue: number];
+  path: Array<[lon: number, lat: number]>;
 }
-
 
 interface IProps {
-  tracks: Track[]
+  tracks: Track[];
 }
 
-
-export function ChallengeMap({tracks}: IProps) {
-  const [viewport, setViewport] = React.useState<WebMercatorViewport>({
-    width: 800,
-    height: 600,
+export function ChallengeMap({ tracks }: IProps) {
+  const [viewState, setViewState] = React.useState<ViewState>({
     latitude: 37.78,
     longitude: -122.45,
-    zoom: 14
-  } as WebMercatorViewport);
+    zoom: 14,
+  });
+  const handleViewStateChange = React.useCallback(
+    (args: { viewState: any }) => {
+      setViewState(args.viewState);
+    },
+    []
+  );
 
   const goToSF = React.useCallback(() => {
-    setViewport((viewport: any) => {
-      const {longitude, latitude, zoom} = new WebMercatorViewport(viewport)
-      .fitBounds([[-122.4, 37.7], [-122.5, 37.8]], {
-          padding: 20,
-          offset: [0, -100]
-        });
-      return {
-      ...viewport,
-      longitude,
-      latitude,
-      zoom,
-      transitionDuration: 5000,
-      transitionInterpolator: new FlyToInterpolator(),
-      // transitionEasing: d3.easeCubic
-    } 
-  }) }, [])
+    setViewState((viewport: ViewState) => {
+      // When deck-gl is initialized the viewport contains additional keys width and height
+      // which are required to create a WebMercatorViewport and calculate bounds.
+      if ("width" in viewport && "height" in viewport) {
+        const { longitude, latitude, zoom } = new WebMercatorViewport(
+          viewport as ViewState & { width: number; height: number }
+        ).fitBounds(
+          [
+            [-122.4, 37.7],
+            [-122.5, 37.8],
+          ],
+          {
+            padding: 20,
+            offset: [0, -100],
+          }
+        );
+        return {
+          ...viewport,
+          longitude,
+          latitude,
+          zoom,
+          transitionDuration: 5000,
+          transitionInterpolator: new FlyToInterpolator(),
+          // transitionEasing: d3.easeCubic
+        };
+      } else {
+        return viewport;
+      }
+    });
+  }, []);
 
   const layers = [
     new PathLayer({
-          id:"path-layer",
-          data: tracks,
-          pickable:true,
-          // widthScale:20,
-          widthMinPixels: 2,
-          getPath: (d: any) => d.path,
-          getColor:d => d.color,
-          getWidth:d => 5,
-
-    })
-  ]
+      id: "path-layer",
+      data: tracks,
+      pickable: true,
+      widthMinPixels: 2,
+      getPath: (d) => d.path,
+      getColor: (d) => d.color,
+      getWidth: (d) => 5,
+    }),
+  ];
 
   return (
     <div>
-      <DeckGL initialViewState={{
-          longitude: viewport.longitude,
-          latitude: viewport.latitude,
-          zoom: viewport.zoom,
-          pitch: 0,
-          bearing: 0
-        }} controller layers={layers}
+      <DeckGL
+        controller
+        layers={layers}
+        viewState={viewState}
+        onViewStateChange={handleViewStateChange}
+      >
+        <InteractiveMap
+          {...viewState}
+          mapboxApiAccessToken={
+            "pk.eyJ1IjoiaXJvbmVzOTQiLCJhIjoiY2txenprY2YzMW4yaDJ2bGZrb3ozbXRzMSJ9.DaLs0HwX916WhZ0f3Z9VKw"
+          }
         >
-        <InteractiveMap {...viewport} onViewportChange={setViewport as any} mapboxApiAccessToken={'pk.eyJ1IjoiaXJvbmVzOTQiLCJhIjoiY2txenprY2YzMW4yaDJ2bGZrb3ozbXRzMSJ9.DaLs0HwX916WhZ0f3Z9VKw'}>
-          <Marker latitude={37.68493} longitude={-122.49050} offsetLeft={-20} offsetTop={-10}>
-            <img style={{"height": "50px"}} src={flatpin} alt="pin"/>
+          <Marker
+            latitude={37.68493}
+            longitude={-122.4905}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <img style={{ height: "50px" }} src={flatpin} alt="pin" />
           </Marker>
-          <Marker latitude={37.7866555224718} longitude={-122.41737904607598} offsetLeft={-20} offsetTop={-10}>
-            <img style={{"height": "50px"}} src={flatpin} alt="pin"/>
+          <Marker
+            latitude={37.7866555224718}
+            longitude={-122.41737904607598}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <img style={{ height: "50px" }} src={flatpin} alt="pin" />
           </Marker>
         </InteractiveMap>
         <button onClick={goToSF}>San Francisco</button>
