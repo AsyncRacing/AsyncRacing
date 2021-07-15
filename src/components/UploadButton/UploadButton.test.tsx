b0940470281062c4eaa1496e3be802a44e354989
Nolan <noltron000@gmail.com>
2021-07-14 20:50:01 -0400

    UploadButton: removing component test (for now)

--- src/components/UploadButton/UploadButton.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import UploadButton from './UploadButton';
-
-test('Confirms that math works', () => {
-  render(<UploadButton setFile={(e)=>{console.log(1+1)}}/>);
-  expect(1+1).toEqual(2);
-});
