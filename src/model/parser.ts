2e0bf4b691307f575c3e5db3105c13f08a82a003
Nolan <noltron000@gmail.com>
2021-07-19 23:43:56 -0400

    Linting parser.ts

--- src/model/parser.ts
+++ src/model/parser.ts
@@ -1,30 +1,23 @@
-// interface TrackPoint {
-//     latitude: number
-//     longitude: number
-//     time: number
-// }
-
-// export const parseGpxData = (data: string) : TrackPoint[] => {
-//     console.log(data)
-//     return []
-// }
-
-import gpxParser, { Track } from 'gpxparser';
-import fs from 'fs';
+import GpxParser from 'gpxparser';
 
 interface TrackPoint {
-    lat: number
-    lon: number
-    time: Date
+  lat: number
+  lon: number
+  time: Date
 }
 
-export function parseGpxData(data: string): TrackPoint[] {
-  const gpx = new gpxParser();
+const parseGpxData = (data: string): TrackPoint[] => {
+  const gpx = new GpxParser();
   gpx.parse(data);
 
-  return gpx.tracks[0].points
-    .map((point) => ({ lat: point.lat, lon: point.lon, time: point.time }));
-}
+  return gpx
+    .tracks[0]
+    .points
+    .map((point) => ({
+      lat: point.lat,
+      lon: point.lon,
+      time: point.time,
+    }));
+};
 
-const file = fs.readFileSync('./ben1.gpx').toString();
-console.log(parseGpxData(file));
+export { parseGpxData };
