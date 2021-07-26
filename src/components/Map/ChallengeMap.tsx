c5f8b7532c4ff62e5201966e4432ce362a3ea6ea
Nolan <noltron000@gmail.com>
2021-07-26 17:41:19 -0400

    Comments & minor tweaks

--- src/components/Map/ChallengeMap.tsx
+++ src/components/Map/ChallengeMap.tsx
@@ -1,28 +1,33 @@
 // module imports
-import * as React from 'react';
-import { InteractiveMap } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
+import React, {
+  useCallback,
+  useState,
+} from 'react';
+import { InteractiveMap } from 'react-map-gl';
 import { PathLayer } from '@deck.gl/layers';
 import { ViewState } from 'react-map-gl/src/mapbox/mapbox';
 
 // local imports
 import { Track } from '../../model/ChallengeConfiguration';
 
+// interfaces & types
 interface PropTypes {
   tracks: Track[];
 }
 
+// react component
 const ChallengeMap = ({ tracks }: PropTypes) => {
+  // This defaults to showing San Francisco.
   const defaultView: ViewState = {
     latitude: 37.78,
     longitude: -122.45,
     zoom: 14,
   };
+  const [viewState, setViewState] = useState<ViewState>(defaultView);
 
-  const [viewState, setViewState] = React.useState<ViewState>(defaultView);
-
-  const handleViewStateChange = React.useCallback(
-    (args: { viewState: any }) => {
+  const handleViewStateChange = useCallback(
+    (args: { viewState: ViewState }) => {
       setViewState(args.viewState);
     },
     [],
