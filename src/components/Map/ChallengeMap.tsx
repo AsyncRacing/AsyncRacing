8a61b5b5ab5800614fe7c849894757480e27ec72
Nolan <noltron000@gmail.com>
2021-07-26 15:36:38 -0400

    Removing functionality testing from ChallengeMap

--- src/components/Map/ChallengeMap.tsx
+++ src/components/Map/ChallengeMap.tsx
@@ -1,18 +1,11 @@
 // module imports
 import * as React from 'react';
-import {
-  InteractiveMap,
-  FlyToInterpolator,
-  WebMercatorViewport,
-  Marker,
-} from 'react-map-gl';
+import { InteractiveMap } from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import { PathLayer } from '@deck.gl/layers';
-// import d3 from 'd3-ease'; // 3rd-party easing functions
 import { ViewState } from 'react-map-gl/src/mapbox/mapbox';
 
 // local imports
-import flatpin from './assets/images/red-pin.png';
 import { Track } from '../../model/ChallengeConfiguration';
 
 interface PropTypes {
@@ -34,39 +27,6 @@ const ChallengeMap = ({ tracks }: PropTypes) => {
     },
     [],
   );
-
-  const goToSF = React.useCallback(() => {
-    setViewState((viewport: ViewState) => {
-      // When deck-gl is initialized the viewport contains additional keys width and height
-      // which are required to create a WebMercatorViewport and calculate bounds.
-      if ('width' in viewport && 'height' in viewport) {
-        const { longitude, latitude, zoom } = new WebMercatorViewport(
-          viewport as ViewState & { width: number; height: number },
-        ).fitBounds(
-          [
-            [-122.4, 37.7],
-            [-122.5, 37.8],
-          ],
-          {
-            padding: 20,
-            offset: [0, -100],
-          },
-        );
-
-        return {
-          ...viewport,
-          longitude,
-          latitude,
-          zoom,
-          transitionDuration: 5000,
-          transitionInterpolator: new FlyToInterpolator(),
-          // transitionEasing: d3.easeCubic
-        };
-      }
-
-      return viewport;
-    });
-  }, []);
 
   const layers = [
     new PathLayer({
@@ -95,38 +55,7 @@ const ChallengeMap = ({ tracks }: PropTypes) => {
       <InteractiveMap
         {...viewState}
         mapboxApiAccessToken="pk.eyJ1IjoiaXJvbmVzOTQiLCJhIjoiY2txenprY2YzMW4yaDJ2bGZrb3ozbXRzMSJ9.DaLs0HwX916WhZ0f3Z9VKw"
-      >
-        <Marker
-          latitude={37.68493}
-          longitude={-122.4905}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <img
-            style={{ height: '50px' }}
-            src={flatpin}
-            alt="pin"
-          />
-        </Marker>
-        <Marker
-          latitude={37.7866555224718}
-          longitude={-122.41737904607598}
-          offsetLeft={-20}
-          offsetTop={-10}
-        >
-          <img
-            style={{ height: '50px' }}
-            src={flatpin}
-            alt="pin"
-          />
-        </Marker>
-      </InteractiveMap>
-      <button
-        type="button"
-        onClick={goToSF}
-      >
-        San Francisco
-      </button>
+      />
     </DeckGL>
   );
 };
