d9e75cb84832268965b8f6164e9563e2d217e4e6
Nolan <noltron000@gmail.com>
2021-07-28 00:59:33 -0400

    Drawing SVGOverlay for start & finish points prototype

--- src/components/Circuit/Waypoint/Waypoint.tsx
+++ src/components/Circuit/Waypoint/Waypoint.tsx
@@ -1,42 +1,51 @@
-// module imports
+/* module imports */
 import React from 'react';
-import { Marker } from 'react-map-gl';
+import { SVGOverlay } from 'react-map-gl';
 
-// local imports
-import { GPSLine } from '../../../model/ChallengeConfiguration';
-import flatpin from '../../../assets/red-pin.png';
+/* local imports */
+import {
+  GPSLine,
+} from '../../../model/ChallengeConfiguration';
 
+/* interfaces & types */
 interface PropTypes {
   line: GPSLine
 }
 
-const Waypoint = ({ line: coordinates }: PropTypes) => {
-  const { firstPoint, secondPoint } = coordinates;
-  return (
-    <>
-      <Marker
-        latitude={firstPoint.lat}
-        longitude={firstPoint.lon}
-      >
-        <img
-          style={{ height: '50px' }}
-          src={flatpin}
-          alt="pin"
-        />
-      </Marker>
+/* helpers & constants */
+// This will initialize a challenge from a couple of lines.
+const getRedrawFx = (line: GPSLine) => {
+  // Deconstruct the given GPSLine.
+  const { firstPoint, secondPoint } = line;
 
-      <Marker
-        latitude={secondPoint.lat}
-        longitude={secondPoint.lon}
-      >
-        <img
-          style={{ height: '50px' }}
-          src={flatpin}
-          alt="pin"
-        />
-      </Marker>
-    </>
-  );
+  // This function must be updated with our line ppoints every time it changes.
+  const redraw = ({ project }: { project: any }) => {
+    // The `project()` function is sort of a "magic" function that ReactMapGL
+    //   passes in as a parameter to the redraw function.
+    // Given an input of lon/lat coords on the globe,
+    //   it converts it to HTML-readable X/Y coords on the page.
+    const [x1, y1] = project([firstPoint.lon, firstPoint.lat]);
+    const [x2, y2] = project([secondPoint.lon, secondPoint.lat]);
+
+    // The app expects redraw to return SVG-compatible JSX elements.
+    return (
+      <line
+        x1={x1}
+        y1={y1}
+        x2={x2}
+        y2={y2}
+        stroke="blue"
+        strokeWidth="15"
+      />
+    );
+  };
+
+  // getRedrawFx returns the updated function.
+  return redraw;
 };
 
+/* react component */
+const Waypoint = ({ line }: PropTypes) => (
+  <SVGOverlay redraw={getRedrawFx(line)} />
+);
 export { Waypoint };
