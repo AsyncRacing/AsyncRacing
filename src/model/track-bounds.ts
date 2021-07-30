e01f347592854b63559a53a948f53fedaff2b4c6
Nolan <noltron000@gmail.com>
2021-07-30 19:32:13 -0400

    Prettifying track bounds
    There are some type errors on this branch, but that will come in the next commit. Only prettier auto-format was applied here. Ran --no-verify to commit anyway.

--- src/model/track-bounds.ts
+++ src/model/track-bounds.ts
@@ -1,49 +1,50 @@
-import { TrackPath } from './ChallengeConfiguration';
+import { TrackPath } from './ChallengeConfiguration'
 
 // eslint-disable-next-line max-len
 
-export const trackBounds = (path: TrackPath): [northEastCorner: [number, number],
-southWestCorner: [number, number]] => {
+export const trackBounds = (
+  path: TrackPath,
+): [northEastCorner: [number, number], southWestCorner: [number, number]] => {
   if (path.length > 0) {
     // eslint-disable-next-line no-shadow
     const eastPoint = path.reduce((accumulator, currentPoint) => {
-      const currentValue = currentPoint.lat;
-      const accumulatorValue = accumulator.lat;
+      const currentValue = currentPoint.lat
+      const accumulatorValue = accumulator.lat
       if (accumulatorValue > currentValue) {
-        return accumulator;
+        return accumulator
       }
-      return currentPoint;
-    });
+      return currentPoint
+    })
     const westPoint = path.reduce((accumulator, currentPoint) => {
-      const currentValue = currentPoint.lat;
-      const accumulatorValue = accumulator.lat;
+      const currentValue = currentPoint.lat
+      const accumulatorValue = accumulator.lat
       if (accumulatorValue < currentValue) {
-        return accumulator;
+        return accumulator
       }
-      return currentPoint;
-    });
+      return currentPoint
+    })
     const northPoint = path.reduce((accumulator, currentPoint) => {
-      const currentValue = currentPoint.lon;
-      const accumulatorValue = accumulator.lon;
+      const currentValue = currentPoint.lon
+      const accumulatorValue = accumulator.lon
       if (accumulatorValue > currentValue) {
-        return accumulator;
+        return accumulator
       }
-      return currentPoint;
-    });
+      return currentPoint
+    })
     const southPoint = path.reduce((accumulator, currentPoint) => {
-      const currentValue = currentPoint.lon;
-      const accumulatorValue = accumulator.lon;
+      const currentValue = currentPoint.lon
+      const accumulatorValue = accumulator.lon
       if (accumulatorValue < currentValue) {
-        return accumulator;
+        return accumulator
       }
-      return currentPoint;
-    });
+      return currentPoint
+    })
 
-    const northEastCorner = [eastPoint.lat, northPoint.lon] as [number, number];
-    const southWestCorner = [westPoint.lat, southPoint.lon] as [number, number];
+    const northEastCorner = [eastPoint.lat, northPoint.lon] as [number, number]
+    const southWestCorner = [westPoint.lat, southPoint.lon] as [number, number]
 
-    return [northEastCorner, southWestCorner];
+    return [northEastCorner, southWestCorner]
   }
 
-  throw new Error('invalid: track is empty');
-};
+  throw new Error('invalid: track is empty')
+}
