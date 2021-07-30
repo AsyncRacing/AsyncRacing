e9123ed3277d202f36afda880d74a9d30a3fc47b
Nolan <noltron000@gmail.com>
2021-07-30 18:28:58 -0400

    Fixing warning about exhaustive deps in useCallback()

--- src/components/Course/ChallengeCourse.tsx
+++ src/components/Course/ChallengeCourse.tsx
@@ -27,29 +27,6 @@ interface WaypointProps {
 }
 
 /* helpers & constants */
-// This will initialize a challenge from a couple of lines.
-const getRedrawFx = (line: GPSLine) => {
-  // Deconstruct the given GPSLine.
-  const { firstPoint, secondPoint } = line
-
-  // This function must be updated with our line ppoints every time it changes.
-  const redraw = ({ project }: { project: any }) => {
-    // The `project()` function is sort of a "magic" function that ReactMapGL
-    //   passes in as a parameter to the redraw function.
-    // Given an input of lon/lat coords on the globe,
-    //   it converts it to HTML-readable X/Y coords on the page.
-    const [x1, y1] = project([firstPoint.lon, firstPoint.lat])
-    const [x2, y2] = project([secondPoint.lon, secondPoint.lat])
-
-    // The app expects redraw to return SVG-compatible JSX elements.
-    return (
-      <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="blue" strokeWidth="15" />
-    )
-  }
-
-  // getRedrawFx returns the updated function.
-  return redraw
-}
 
 /* react components */
 // helper component
@@ -84,7 +61,39 @@ const Pin = ({ point, setPoint }: PinProps) => {
 
 // helper component
 const Waypoint = ({ line, setLine }: WaypointProps) => {
-  const redraw = useCallback(getRedrawFx(line), [line])
+  const redraw = useCallback(
+    // This will initialize a challenge from a couple of GPS lines.
+    // It latches into the SVGOverlay component.
+    () => {
+      // Deconstruct the given GPSLine.
+      const { firstPoint, secondPoint } = line
+
+      // This function must be updated with our line ppoints every time it changes.
+      const redraw = ({ project }: { project: any }) => {
+        // The `project()` function is sort of a "magic" function that ReactMapGL
+        //   passes in as a parameter to the redraw function.
+        // Given an input of lon/lat coords on the globe,
+        //   it converts it to HTML-readable X/Y coords on the page.
+        const [x1, y1] = project([firstPoint.lon, firstPoint.lat])
+        const [x2, y2] = project([secondPoint.lon, secondPoint.lat])
+
+        // The app expects redraw to return SVG-compatible JSX elements.
+        return (
+          <line
+            x1={x1}
+            y1={y1}
+            x2={x2}
+            y2={y2}
+            stroke="blue"
+            strokeWidth="15"
+          />
+        )
+      }
+      // getRedrawFx returns the updated function.
+      return redraw
+    },
+    [line],
+  )
 
   const getSetPointOf = useCallback(
     (name: string) => (point: GPSPoint) => setLine({ ...line, [name]: point }),
