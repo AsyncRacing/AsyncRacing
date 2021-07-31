ac63f82961894de233c48038d20910a8c4a34104
Ian Rones <ronesp.85@gmail.com>
2021-07-31 11:14:19 -0700

    Added turf.js & working on Timer.tsx

---
+++ src/components/Timer.tsx
@@ -0,0 +1,11 @@
+import { TrackPath, Challenge } from '../model/ChallengeConfiguration'
+import intersect from '@turf/line-intersect'
+
+interface GeoJson {
+  lineOne: TrackPath[]
+  lineTwo: TrackPath[]
+}
+
+const Timer = () => {}
+
+export { Timer }
