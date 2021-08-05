24f074db01ae203afa29ef824b276b11549fa9ce
Nolan <noltron000@gmail.com>
2021-08-04 20:05:57 -0400

    Moving tracks .map method logic

--- src/components/GetMapTrack/GetMapTrack.tsx
+++ src/components/GetMapTrack/GetMapTrack.tsx
@@ -1,32 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ChallengeMap } from '../Map/ChallengeMap'
 import { UploadButton } from '../UploadButton/UploadButton'
 import { useFiles } from '../../model/useFiles'
 import { useFilesToTextMap } from '../../model/useFileToText'
 import { useFilesToTrackMap } from '../../model/useTextToPath'
+import { Track } from '../../model/ChallengeConfiguration'
+import { useEffect } from 'react'
 
 const GetMapTrack = () => {
   const [files, , addFiles, clearFiles] = useFiles()
   // file upload manipulation
   const fileTextMap = useFilesToTextMap(files)
   const filePathMap = useFilesToTrackMap(files, fileTextMap)
+  const [tracks, setTracks] = useState<Array<Track>>([])
+  useEffect(() => {
+    console.log('TRANSFORMING STEP!')
+    console.log(filePathMap)
+    setTracks(
+      [...filePathMap.entries()]
+        .map(([file, trackPaths]) => {
+          return trackPaths.map((trackPath) => ({
+            name: file.name,
+            path: trackPath,
+            color: [0, 0, 255] as [red: number, green: number, blue: number],
+          }))
+        })
+        .flat(1),
+    )
+  }, [filePathMap])
   return (
     <>
-      <ChallengeMap
-        tracks={[...filePathMap.entries()]
-          .map(([file, trackPaths]) => {
-            return trackPaths.map((trackPath) => ({
-              name: file.name,
-              path: trackPath,
-              color: [
-                255 * Math.random(),
-                255 * Math.random(),
-                255 * Math.random(),
-              ] as [red: number, green: number, blue: number],
-            }))
-          })
-          .flat(1)}
-      />
+      <ChallengeMap tracks={tracks} />
 
       <div
         style={{
