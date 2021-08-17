ec149bf046817a45f46c63ff3e9be428dc8ae5c7
Nolan <noltron000@gmail.com>
2021-08-17 13:14:21 -0400

    Configure firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,31 @@
+// Libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
+import firebase from 'firebase'
+
+// Local imports
 import './index.css'
 import { Home } from './pages/Home/Home'
 import { reportWebVitals } from './reportWebVitals'
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: 'AIzaSyCiT4b8_qyC2ZxF95aRAdE8j4Kej3Dt9kk',
+  authDomain: 'asyncracing-d5302.firebaseapp.com',
+  databaseURL: 'https://asyncracing-d5302-default-rtdb.firebaseio.com',
+  projectId: 'asyncracing-d5302',
+  storageBucket: 'asyncracing-d5302.appspot.com',
+  messagingSenderId: '468547346614',
+  appId: '1:468547346614:web:0a5a388705b91b5b088617',
+  measurementId: 'G-199TR40228',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+// Initialize React
 ReactDOM.render(
   <React.StrictMode>
     <Home />
