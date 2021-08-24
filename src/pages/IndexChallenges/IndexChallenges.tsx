e149c374a41dc8810cd8b44e2c695717f3f6be59
Nolan <noltron000@gmail.com>
2021-08-24 16:31:25 -0400

    Using react hooks in the index page

--- src/pages/IndexChallenges/IndexChallenges.tsx
+++ src/pages/IndexChallenges/IndexChallenges.tsx
@@ -1,38 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Challenge } from '../../model/ChallengeConfiguration'
+import { useObjectVal } from 'react-firebase-hooks/database'
+import { ChallengeSchema } from '../../model/ChallengeConfiguration'
 import { firebaseDB } from '../../model/firebase-config'
 
 const IndexChallenges = () => {
   // get challenges from firebase DB
-  const [challengeDB, setChallengeDB] = useState<Record<string, Challenge>>({})
-  //const challengeDBRef = firebaseDB.ref(`challenges/${challengeID}`)
-  useEffect(() => {
-    ;(async () => {
-      const challengesFromDB = (await firebaseDB.ref('challenges').get()).val()
-      Object.values(challengesFromDB).forEach((challenge: any) => {
-        challenge.tracks = {}
-        challenge.tracks = Object.values(challenge.tracks)
-      })
-      setChallengeDB(challengesFromDB)
-    })()
-  })
+  const [challenges, loading, error] = useObjectVal<ChallengeSchema>(
+    firebaseDB.ref('challenges'),
+  )
 
   return (
     <>
       <h2>Home</h2>
-      <ul>
-        {Object.entries(challengeDB).map(([challengeID, challenge]) => {
-          return (
-            <li>
-              <Link to={`/challenges/${challengeID}`}>
-                Challenge: {challenge.metadata.title}
-                <ul>Creator: {challenge.metadata.creator}</ul>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+      {challenges && (
+        <ul>
+          {Object.entries(challenges).map(([challengeID, challenge]) => {
+            return (
+              <li>
+                <Link to={`/challenges/${challengeID}`}>
+                  <span>
+                    Challenge: {challenge.metadata.title} <br />
+                    Creator: {challenge.metadata.creator}
+                  </span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+      {loading && (
+        <p>
+          <em>Loading Database...</em>
+        </p>
+      )}
+      {error && (
+        <p>
+          <strong>Database Error!</strong>
+        </p>
+      )}
+
       <Link to="/challenges/new">New Challenge</Link>
     </>
   )
